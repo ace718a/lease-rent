dd7e0662a65639d2934f3f03f3ee737037830c9e
ace718a <ace718@naver.com>
2026-06-23 23:02:42 +0900

    Remove unused imports and Home component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,29 +15,3 @@ export default function NotFound() {
     </div>
   );
 }
-  import Header from "./components/Header";
-import Hero from "./components/Hero";
-import ServicesShowcase from "./components/ServicesShowcase";
-import WhyMoapick from "./components/WhyMoapick";
-import ServiceCTA from "./components/ServiceCTA";
-import InfoNewsPreview from "./components/InfoNewsPreview";
-import FAQ from "./components/FAQ";
-import CTABand from "./components/CTABand";
-import Footer from "./components/Footer";
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-background-50 text-foreground-900 font-body">
-      <Header />
-      <main>
-        <Hero />
-        <ServicesShowcase />
-        <WhyMoapick />
-        <ServiceCTA />
-        <InfoNewsPreview />
-        <FAQ />
-        <CTABand />
-      </main>
-      <Footer />
-    </div>
-  );
-}
